3a309942ebcda47cfc686b3302c4ee065b148cda
qzcsgs <746515766@qq.com>
2018-08-01 18:37:31 +0800

    Game.js add import

--- src/com.eatBallGame.qzc/js/Game.js
+++ src/com.eatBallGame.qzc/js/Game.js
@@ -1,3 +1,11 @@
+import CONFIG from './CONFIG'
+import UTIL from './UTIL'
+import Map from './Map'
+import AiPlayer from './AiPlayer'
+import Food from './Food'
+import Player from './Player'
+import Vomit from './Vomit'
+
 /**
  * 游戏类
  */
